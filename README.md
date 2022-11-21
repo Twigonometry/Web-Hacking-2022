7b87b54ba0aeb6c5d0fb7df5e8a659e53f03bd50
Twigonometry <mac.j.goodwin@gmail.com>
2022-11-21 12:25:44 +0000

    Uncommitted changes from deployment on day

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Web Hacking demo for SESH, 2022 edition!
 ### Windows
 
 ```
-PS > cd .\web\
+PS > cd .\app\
 PS > $env:FLASK_APP = "app"
 PS > $env:FLASK_DEBUG = "true"
 PS > python -m flask run
--- app/app.py
+++ app/app.py
@@ -181,5 +181,4 @@ def cookies():
 
 if __name__ == '__main__':
     init_db()
-    app.run(host="127.0.0.1", port=5000, debug=True)
-    # app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
